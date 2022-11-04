634e7b7fa6b0cd001c5d8751b1aefd6a6fb85404
PMuin <99225997+PMuin@users.noreply.github.com>
2022-11-04 20:29:51 +0100

    first commit

---
+++ src/Components/Form.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import InputElement from "./InputElement";
+
+class Form extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state={
+           ok:true,
+           display: '' 
+        }
+    }
+    render() {
+        let message;
+        if (this.state.ok === true) {
+            message = "All Ok";
+        } else {
+            message = "Not Ok"
+        }
+    return (
+      <div>
+        <h1>Form {this.props.data} </h1>
+        <form> 
+            <InputElement label='Username' type='text' value={user.name}/>
+            <InputElement label='Password' type="text" value={user.password}/>
+          
+          <label>Password</label>
+          <input type="password" />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{'message:' + message}</p>
+        <p>{user.username}</p>
+      </div>
+    );
+  }
+}
+export default Form;
---
+++ src/Components/InputElement.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class InputElement extends Component {
+  render() {
+    return (
+      <div>
+        <label>
+            {this.props.label}
+        </label>
+        <input 
+        type={this.props.type}
+        value={this.props.value}
+        />
+      </div>
+    )
+  }
+}
+
+export default InputElement
